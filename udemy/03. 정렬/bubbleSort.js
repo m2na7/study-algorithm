dc72408f72144d6621982aacff3d65e277fcd734
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-01-12 21:01:29 +0900

    section11

---
+++ udemy/03. 정렬/bubbleSort.js
@@ -0,0 +1,32 @@
+function bubbleSort2(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// 최적화
+function bubbleSort(arr) {
+  var noSwaps;
+  for (var i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (var j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
