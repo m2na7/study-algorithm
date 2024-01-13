9e3217365b4224703e5f0d182ae565bfdca4f5d5
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-01-13 23:55:41 +0900

    section15

---
+++ udemy/03. 정렬/mergeSort.js
@@ -0,0 +1,35 @@
+// 정렬된 두개의 배열을 합병
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+// console.log(merge([100, 200], [1, 2, 3, 5, 6]));
+console.log(mergeSort(mergeSort([10, 24, 76, 73])));
